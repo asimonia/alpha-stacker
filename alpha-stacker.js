eace92b2d63d839c0d3d4ed0bea1da1db9a94f54
asimonia <alex.simonian@gmail.com>
2016-10-06 14:04:41 -0500

    Add alpha stacker

---
+++ alpha-stacker.js
@@ -0,0 +1,26 @@
+// Create an array that contains the letters of the alphabet
+var alphabet = ['a', 'b', 'c', 'd', 'e', 
+				'f', 'g', 'h', 'i', 'j', 
+				'k', 'l', 'm', 'n', 'o', 
+				'p', 'q', 'r', 's', 't', 
+				'u', 'v', 'w', 'x', 'y', 
+				'z'];
+
+// The stackLetter function should accept the array as the sole argument
+function stackLetters(theAlphabetArray) {
+	var alphaString = '';
+	var counter = 0;
+
+	for (var i = 0; i <= theAlphabetArray.length - 1; i++) {
+		alphaString += theAlphabetArray[i];
+		counter++;
+
+		if ( !(counter % 3) ) {
+			alphaString += " ";
+		}
+		
+		console.log(alphaString);
+	}
+
+}
+stackLetters(alphabet);
